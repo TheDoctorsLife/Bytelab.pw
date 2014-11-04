242142bc0a091f240a13820d8c505ad9775dae22
TheDoctorsLife <hoffmeyer25@gmail.com>
2014-11-03 23:42:03 -0500

    Add in  target="_blank"
    Above title^ This makes the link open in a new window

--- index.php
+++ index.php
@@ -60,13 +60,13 @@
             <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
                 <a class="pure-menu-heading" href>ByteLab</a>
                 <ul>
-                    <li><a href="https://github.com/Byte-Lab">
-                            <i class="fa fa-fw fa-github"></i>
+                    <li><a href="https://github.com/Byte-Lab" target="_blank">
+                            <i class="fa fa-fw fa-github" target="_blank"></i>
                             Source</a></li>
-                    <li><a href="http://ci.bytelab.pw">
+                    <li><a href="http://ci.bytelab.pw" target="_blank">
                             <i class="fa fa-fw fa-tasks"></i>
                             CI</a></li>
-                    <li><a href="http://jd.bytelab.pw">
+                    <li><a href="http://jd.bytelab.pw" target="_blank">
                             <i class="fa fa-fw fa-folder-open"></i>
                             Javadocs</a></li>
                 </ul>
@@ -91,7 +91,7 @@
                     <div class="pure-u-1-3">
                         <div class="l-box">
                             <h3 class="content-subhead">
-                                <a href="https://github.com/Byte-Lab/Warbands">
+                                <a href="https://github.com/Byte-Lab/Warbands" target="_blank">
                                 <i class="fa fa-fw fa-asterisk"></i>
                                 Warbands</h3></a>
                             <p>Warbands is an alternative to the popular plugin Factions, designed to be more modular
@@ -121,7 +121,7 @@
             <div class="ribbon l-box-lrg">
                 <h2 class="content-head content-head-ribbon is-center">Need to get ahold of us?</h2>
                 <p class="is-center"><i>That's alright, because we like hearing from you! Go ahead and fill out the form
-                        below, or <a href="mailto:contact@bytelab.pw">email us</a>!</i></p>
+                        below, or <a href="mailto:contact@bytelab.pw" target="_blank">email us</a>!</i></p>
 
                 <form class="pure-form pure-form-stacked is-center"
                       action="index.php" method="post">
